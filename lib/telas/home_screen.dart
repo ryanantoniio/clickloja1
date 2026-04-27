07e69b4b1aef710454963209b7b0574e2972a6d1
Ryan Antonio <raas3@aluno.ifal.edu.br>
2026-04-27 10:52:38 -0300

    melhoria tela inicial

--- lib/telas/home_screen.dart
+++ lib/telas/home_screen.dart
@@ -11,29 +11,41 @@ class HomeScreen extends StatelessWidget {
         title: const Text('ClickLoja - Início', style: TextStyle(color: Colors.black54)),
         backgroundColor: Colors.white54,
         elevation: 0,
+
+        // icone de carrinho ao lado do nome
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_bag, color: Colors.black54),
+            onPressed: (){
+            },
+          ),
+        ],
       ),
-      body: ListView(
+      body: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 0.7,
         padding: const EdgeInsets.all(16.0),
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
         children: [
           _buildProdutoCard(
             context,
             'Camisa Seleção Brasileira',
             'R\$ 49,99',
-            Colors.blueAccent,
+            'https://images.tcdn.com.br/img/img_prod/690558/camisa_selecao_brasileira_i_2022_torcedor_nike_masculina_amarela_3012_1_eec12ed4121b1f6db2256da6f2bc8f8c.jpg',
           ),
-          const SizedBox(height: 16),
           _buildProdutoCard(
             context,
             'Shorts Masculino',
             'R\$ 29,99',
-            Colors.grey,
+            'https://img.ltwebstatic.com/images3_pi/2021/08/09/1628495047f3f1e98031d200fc01e237db8754b2c1_thumbnail_600x.webp',
           ),
         ],
       ),
     );
   }
 
-  Widget _buildProdutoCard(BuildContext context, String titulo, String preco, Color cor) {
+  Widget _buildProdutoCard(BuildContext context, String titulo, String preco, String url) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -46,18 +58,45 @@ class HomeScreen extends StatelessWidget {
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 150,
-              decoration: BoxDecoration(
-                color: cor,
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
-              ),
-              child: const Center(child: Icon(Icons.image, size: 50, color: Colors.white)),
+            // pra imagem ocupar o maximo de espaco
+            Expanded(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                  child: Image.network(
+                    url, // usa  a url da imagem passada no metodo
+                    width: double.infinity,
+                    fit: BoxFit.cover, // garante que a imagem vai preencher todo o espaco
+                    loadingBuilder: (context, child, loadingProgress){
+                      if (loadingProgress == null) return child; // se carregou vai mostrar a imagem
+                      return const Center(child: CircularProgressIndicator()); // se nao vai mostrar o circulo girando
+                    },
+                    errorBuilder: (context, error, stackTrace){
+                      // se der erro mostra o icone de foto
+                      return const Center(child: Icon(Icons.image, size: 50, color: Colors.grey));
+                    },
+                  ),
+                ),
             ),
-            ListTile(
-              title: Text(titulo, style: const TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text(preco, style: const TextStyle(color: Colors.green, fontSize: 16, fontWeight: FontWeight.bold)),
+
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    titulo,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                    maxLines: 2, // limita o texto a 2 linhas
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    preco,
+                    style: TextStyle(color: Colors.green, fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
