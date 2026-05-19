918e1cadde159791d6d0681ea436750d99def1fe
Wallisson <wallissonrss244@gmail.com>
2026-05-19 17:17:53 -0300

    Criei o domain para facilitar o listview

---
+++ lib/domain/propriedade.dart
@@ -0,0 +1,12 @@
+
+ class Propriedade {
+  String urlImagem;
+  String nomeImagem;
+  String preco;
+
+  Propriedade({
+    required this.urlImagem,
+    required this.nomeImagem,
+    required this.preco,
+ })
+ }
