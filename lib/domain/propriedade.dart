8a6f0bee1daa728fb9aa980defc8177866c4a734
ryanantoniio <raas3@aluno.ifal.edu.br>
2026-06-16 17:34:31 -0300

    mod1

--- lib/domain/propriedade.dart
+++ lib/domain/propriedade.dart
@@ -1,12 +1,18 @@
 
  class Propriedade {
-  String urlImagem;
-  String nomeImagem;
+  String titulo;
   String preco;
+  String url;
 
   Propriedade({
-    required this.urlImagem,
-    required this.nomeImagem,
+    required this.titulo,
     required this.preco,
+    required this.url,
  });
+
+  Propriedade.fromJson(Map<String, dynamic> json)
+    : titulo = json['titulo'].toString(),
+      preco = json['preco'].toString(),
+      url = json['url'];
+
  }
